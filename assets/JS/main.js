c30c089e9d564ac0fda215c747d6fe2695fb39d2
yahyajaara <yahyajaara1411@gmail.com>
2024-11-28 16:44:03 +0200

    last edit

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -132,9 +132,24 @@ const swiper = new Swiper('.swiper', {
           document.querySelector(".footswear .footswear-end .number ").textContent = ++index ;
           document.querySelector(".footswear .footswear-end .sum").textContent = ++ArrayPrice[item.index];
 
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "bottom-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: `The shoe  ${index} has been added to your cart`
+          });
+
       });
 
-     
 
     });
 
@@ -143,4 +158,104 @@ const swiper = new Swiper('.swiper', {
     incIndex();
 
 
+    const closeLogInBut = ()=>
+    {
+      const logModel = document.querySelector(".my-LogModel");
+      const close = document.querySelector(".my-LogModel .btn-close");
+      
+      close.addEventListener('click', () => {
+        logModel.classList.add("displayNone");
+      });
+    }
+
+    closeLogInBut();
+
+
+    document.addEventListener('scroll', () => {
+      const elements = document.querySelectorAll('.hidden');
+      const windowHeight = window.innerHeight;
+  
+      elements.forEach(el => {
+          const positionFromTop = el.getBoundingClientRect().top;
+  
+          if (positionFromTop - windowHeight <= -100) {
+              el.classList.add('visible');
+          }
+
+          else {
+              el.classList.remove('visible');
+          }
+      });
+  });
+  
+
+  
+const arrows = ()=>
+{
+  const backToTopButton = document.querySelector('.back-to-top');
+  window.addEventListener('scroll', () => 
+  {
+
+  if (window.scrollY > 400) 
+  { 
+    backToTopButton.style.display = 'block';
+  } 
+
+  else 
+  {
+    backToTopButton.style.display = 'none';
+  }
+
+});
+
+}
+
+arrows();
+
+
+  
+
+
+ const hideNavBar = ()=>
+ {
+
+  let lastScrollPosition = 0;
+
+  const navbar = document.querySelector('.footswear');
+
+  window.addEventListener('scroll', () => 
+  {
+
+  const currentScrollPosition = window.scrollY;
+
+  
+  console.log(lastScrollPosition);
+
+
+  if (currentScrollPosition > lastScrollPosition) 
+  {
+    navbar.classList.add('hiddenNav');
+    lastScrollPosition = currentScrollPosition;
+
+  } 
+
+  else 
+  {
+  
+    navbar.classList.remove('hiddenNav');
+    navbar.classList.add('ScrollNav');
+    lastScrollPosition = currentScrollPosition;
     
+    if (currentScrollPosition == 0)
+    {
+      navbar.classList.remove('ScrollNav');
+    }
+
+  }
+
+  
+  
+});
+
+ }
+ hideNavBar();
